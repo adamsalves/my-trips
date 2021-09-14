4c9d4d84688bc672a3e4cc63ed5d7d858bde0da0
Adams Alves <alves.adams@gmail.com>
2021-09-14 15:46:05 -0300

    Add generic template pages

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,11 +1,21 @@
 import { gql } from 'graphql-request'
 
 export const GET_PAGES = gql`
-  query getPages {
-    pages {
+  query getPages($first: Int) {
+    pages(first: $first) {
       id
       heading
       slug
+    }
+  }
+`
+
+export const GET_PAGE_BY_SLUG = gql`
+  query getPageBySlug($slug: String!) {
+    page(where: { slug: $slug }) {
+      id
+      slug
+      heading
       body {
         html
       }
