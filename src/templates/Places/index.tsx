6343551a4043ba1d6dca92bd2b7ee5cfc06182a2
Adams Alves <alves.adams@gmail.com>
2021-09-15 10:25:06 -0300

    Adjust next image

--- src/templates/Places/index.tsx
+++ src/templates/Places/index.tsx
@@ -1,4 +1,10 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline'
+import LinkWrapper from 'components/LinkWrapper'
+import { useRouter } from 'next/dist/client/router'
 import Image from 'next/image'
+
+import * as S from './style'
+
 type ImageProps = {
   url: string
   height: number
@@ -9,7 +15,7 @@ export type PlacesTemplateProps = {
   place: {
     slug: string
     name: string
-    description: {
+    description?: {
       html: string
     }
     gallery: ImageProps[]
@@ -17,15 +23,37 @@ export type PlacesTemplateProps = {
 }
 
 export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  const router = useRouter()
+
+  if (router.isFallback) return null
+
   return (
     <>
-      <h1>{place.name}</h1>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} />
+      </LinkWrapper>
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
 
-      <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+          <S.Body
+            dangerouslySetInnerHTML={{ __html: place.description?.html || '' }}
+          />
 
-      {place.gallery.map((image, index) => (
-        <Image key={`photo-${index}`} src={image.url} alt={place.name} />
-      ))}
+          <S.Gallery>
+            {place.gallery.map((image, index) => (
+              <Image
+                key={`photo-${index}`}
+                src={image.url}
+                alt={place.name}
+                width={1000}
+                height={600}
+                quality={75}
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
     </>
   )
 }
